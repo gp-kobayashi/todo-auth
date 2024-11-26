e1e60cd650b60ce41ffa2be410e72168d2b58502
gp-kobayashi <kobayashi_y@gp1000.jp>
2024-11-26 13:56:49 +0900

    [add] login,signupの機能追加

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,23 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import Signup from "@/app/components/signup"
+import type { Database } from "../../../../lib/database.types";
+
+const SignupPage = async () => {
+    const supabase = createServerComponentClient<Database>({
+        cookies,
+    })
+
+    const{
+        data:{ session },
+    } = await supabase.auth.getSession()
+
+    if(session){
+        redirect("/")
+    }
+
+    return <Signup />
+}
+
+export default SignupPage
