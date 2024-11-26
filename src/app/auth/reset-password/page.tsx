2f77e9d5f9789c40f620a610b16c22af2fd02d6e
gp-kobayashi <kobayashi_y@gp1000.jp>
2024-11-26 14:17:00 +0900

    [add]パスワードリセット機能の追加

---
+++ src/app/auth/reset-password/page.tsx
@@ -0,0 +1,23 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import ResetPassword from "@/app/components/reset-password"
+import type { Database } from "../../../../lib/database.types";
+
+const ResetPasswordPage = async () => {
+	const supabase = createServerComponentClient<Database>({
+		cookies,
+	})
+
+	const {
+		data: { session },
+	} = await supabase.auth.getSession()
+
+	if(session){
+		redirect("/")
+	}
+
+	return <ResetPassword />
+}
+
+export default ResetPasswordPage
