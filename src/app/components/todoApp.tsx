8bcd85a29b2da1534d0efae3993695fa5501ef0c
gp-kobayashi <kobayashi_y@gp1000.jp>
2024-11-29 16:20:49 +0900

    [add]todoの取得、追加機能の実装

--- src/app/components/todoApp.tsx
+++ src/app/components/todoApp.tsx
@@ -4,10 +4,10 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
 import { useState, useEffect, useCallback } from "react"
 import useStore from "../../../store"
 import type { Database } from "../../../lib/database.types"
-import { number, string } from "zod"
+import TodoList from "@/app/components/todoList"
 
-const todoApp = () =>{
-    const supabase = createClientComponentClient<Database>()
+const TodoApp = () =>{
+    const supabase = createClientComponentClient<Database['public']['Tables']['todos']['Row'][]>()
     const [todos, setTodos] = useState<Database['public']['Tables']['todos']['Row'][]>([])
     const [title, setTitle] = useState<string>("")
     const [message, setMessage] = useState("")
@@ -34,11 +34,11 @@ const todoApp = () =>{
         const handleSubmit = useCallback(async (e : React.FormEvent<HTMLFormElement>) => {
             e.preventDefault();
             if (title === "") return;
-            const addTodo = async () => {
+            const addTodo = async (title:string) => {
                 const {data, error: addTodoError} = await supabase
                     .from('todos')
                     .insert({
-                        title: title,
+                        title,
                         user_id: user.id,
                     })
                     .select();
@@ -46,29 +46,32 @@ const todoApp = () =>{
                     setMessage("エラーが発生しました" + addTodoError.message)
                     return;
                 }
-
                 return data[0];
             };
-            const updatedTodo = await addTodo();
-            setTodos((prevTodos) => [...prevTodos, { ...updatedTodo, user_id:user.id , title, completed:false }]);
+            const updatedTodo = await addTodo(title);
+            if (updatedTodo) {
+                setTodos((prevTodos) => [...prevTodos, updatedTodo]);
+            }
             setTitle("");
             },[title, user.id]);
     
         return(
             <div>
                 <div>
-      <form className="" onSubmit={(e) => handleSubmit(e)}>
-        <input type="text"
-          placeholder="やること…"
-          className=""
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}/>
-        <button className="">
-            記入
-        </button>
-      </form>
-    </div>
+                    <form className="mb-5" onSubmit={(e) => handleSubmit(e)}>
+                        <input type="text"
+                            placeholder="やること…"
+                            className="border rounded-md py-2 px-3 focus:outline-none focus:border-sky-500"
+                            onChange={(e) => setTitle(e.target.value)}
+                            value={title}
+                        />
+                        <button className="font-bold bg-sky-500 hover:brightness-95 rounded-full p-2 text-white text-sm">
+                            記入
+                        </button>
+                    </form>
+                    <TodoList todos={todos} setTodos={setTodos}/>
+                </div>
             </div>
         )
 }
-export default todoApp
+export default TodoApp
